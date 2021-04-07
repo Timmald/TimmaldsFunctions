fe62c8cabd12b087957fe127f46390e7ab435087
Timmald <81472855+Timmald@users.noreply.github.com>
2021-04-06 21:05:45 -0400

    Add files via upload

--- timmaldFunctions/imageStuff/randShift.py
+++ timmaldFunctions/imageStuff/randShift.py
@@ -1,14 +1,19 @@
 import random
+from typing import Union
+
 from PIL.Image import *
 
 
-def randShift(img: Image, extremeness: int = 50):
+def randShift(img: Image, extremeness:Union[int,float] = 50):
     def pixelRandShift(num: int):
-        return num + random.randint(-extremeness,extremeness)
-
+        return num + random.randint(-round(abs(extremeness)),round(abs(extremeness)))
+    if extremeness==0:
+        extremeness=1
     aspect = img.height / img.width
-    newWidth = round((10/extremeness) * img.width)
+    newWidth = round(abs(10/extremeness) * img.width)
     newHeight = round(newWidth * aspect)
-    img = img.resize((newWidth, newHeight))
+    if newHeight==float('inf') and newWidth==float('inf'):
+        newHeight,newWidth=img.height*2,img.width*2
+    img = img.resize((int(newWidth), int(newHeight)))
     return eval(img, pixelRandShift)
     #TODO: set up individual band randshift methods, make an option to separate extremeness from pixellation
