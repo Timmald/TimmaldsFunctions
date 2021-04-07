5396d159a309a60394948de2d6e11094c8ca09b4
Timmald <81472855+Timmald@users.noreply.github.com>
2021-04-07 09:34:22 -0400

    Add files via upload

--- timmaldFunctions/byteStuff/fileReading.py
+++ timmaldFunctions/byteStuff/fileReading.py
@@ -1,11 +1,17 @@
 import os
 
 
-def bytesFromPath(path:str):
+def bytesFromPath(path: str) -> bytes:
+    """
+    Args:
+        path:A string representing the path of the file to get bytes from. If it is a path to a directory, will recur through the directory tree and return the bytes of the first file it finds.
+    Returns:
+        The bytes of a file indicated by the path
+    """
     try:
-        with open(path,"rb") as f:
+        with open(path, "rb") as f:
             lines = f.read()
         return lines
     except IsADirectoryError:
         for file in os.listdir(path):
-            return bytesFromPath(path+'/'+file)
+            return bytesFromPath(path + '/' + file)
--- timmaldFunctions/stringStuff/randString.py
+++ timmaldFunctions/stringStuff/randString.py
@@ -2,10 +2,24 @@ import random
 from typing import Union
 
 
-def randChar():
+def randChar() -> str:
+    """Generates a random character from the alphabet.
+    Takes no parameters.
+
+    Returns
+    ---------
+
+    char: :class: `str`
+        A string of length one containing a letter
+    """
     letters='abcdefghijklmnopqrstuvwxyz'
     return random.choice(letters)
-def randString(len:Union[int,float]):
+def randString(len:Union[int,float]) -> str:
+    """Args:
+            len: Length of the string to generate. If it is a float, will be rounded
+       Returns:
+           A string of length len, filled with random characters
+    """
     returnString=''
     for i in range(int(round(len))):
         returnString+=randChar()
